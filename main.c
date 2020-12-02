fe50fb68266923d1f7161ec8c9c45d00405421eb
Xonha <xonha@pm.me>
2020-12-02 09:30:41 -0300

    zfsadf

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "pilhaEncadeada.h"
-#include "pilhaEncadeada.c"
+//#include "pilhaEncadeada.c"
 
 int main()
 {
-    char exp[] = "33 + 3 - 1";
-    converteInfixoParaPosfixo(exp);
+    // char exp[] = "33 + 3 - 1";
+    char exp[100];
+    scanf("%[^\n]s", &exp);
+    converteInfixo(exp);
     printf("%s\n", exp);
-    printf("%d\n", resolverPosfixo(exp));
+    printf("%d\n", resolvePosfixo(exp));
 
     return 0;
 }
